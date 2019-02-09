e7a530c770c33d144eae6c152da110ea7e34f4dd
Samuel Rocha <samuelrodriguessr@hotmail.com>
2019-02-08 23:11:08 -0300

    starter project spring data example

---
+++ src/main/java/br/com/codenation/Aplicacao.java
@@ -0,0 +1,38 @@
+package br.com.codenation;
+
+import br.com.codenation.models.Produto;
+import br.com.codenation.repositories.ProdutoRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Aplicacao {
+
+    public static void main(String[] args){
+        SpringApplication.run(Aplicacao.class, args);
+    }
+
+    @Bean
+    public String exemplo(ProdutoRepository repositorio) {
+        System.out.println("Cadastrando produtos...");
+        repositorio.save(new Produto("Sabonete", 10, 1.35));
+        repositorio.save(new Produto("Lâmpada", 35, 7.50));
+        repositorio.save(new Produto("Impressora", 20, 350.00));
+        repositorio.save(new Produto("Computador", 10, 2500.00));
+        System.out.println("-------------------------------");
+
+        System.out.println("-------------------------------");
+
+        System.out.println("Produtos cadastrados:");
+        for (Produto produto : repositorio.findAll()) {
+            System.out.println(produto.toString());
+        }
+        System.out.println("-------------------------------");
+
+        return "";
+    }
+
+
+
+}
---
+++ src/main/java/br/com/codenation/repositories/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package br.com.codenation.repositories;
+
+import br.com.codenation.models.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    Produto findByNomeAndQuantidade(String nome, int quantidade);
+
+}
+
+
+
