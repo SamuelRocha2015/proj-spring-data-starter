1c916f885f9a9d14a6222e290f14092b2e104742
Samuel Rocha <samuelrodriguessr@hotmail.com>
2019-02-08 23:12:32 -0300

    add query find by name

--- src/main/java/br/com/codenation/repositories/ProdutoRepository.java
+++ src/main/java/br/com/codenation/repositories/ProdutoRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
     Produto findByNomeAndQuantidade(String nome, int quantidade);
+    Produto findByNome (String nome );
 
 }
 
